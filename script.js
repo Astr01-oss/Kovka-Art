b29c86b1a0ff1f780df21f3b02ebfcb1b590c9ca
Astr01-oss <gaponovynikitaidana@gmail.com>
2025-05-21 10:14:54 +0300

    1

--- script.js
+++ script.js
@@ -11,3 +11,33 @@ const swiperText = new Swiper('.swiper', {
         delay: 3000,
     },
 })
+
+function hoverOn(){
+    document.getElementById('training_choose-ul1').classList.add('visible');
+    document.getElementById('butt1').classList.add('visible');
+
+}
+function hoverOff(){
+    document.getElementById('training_choose-ul1').classList.remove('visible');
+    document.getElementById('butt1').classList.remove('visible');
+}
+function hoverOn2(){
+    document.getElementById('training_choose-ul2').classList.add('visible');
+    document.getElementById('butt2').classList.add('visible');
+
+}
+function hoverOff2(){
+    document.getElementById('training_choose-ul2').classList.remove('visible');
+    document.getElementById('butt2').classList.remove('visible');
+}
+
+function reviews(){
+    alert("Отзывы пока что, не доступны :3")
+}
+
+function contact(){
+    document.getElementById('contact').classList.remove('hidden');
+}
+function hide(){
+    document.getElementById('contact').classList.add('hidden');
+}
